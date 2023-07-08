773185e0e7a7d0083a69020ad596d0ad7499ce62
Andrew Martins <andrew_omartins@yahoo.com>
2023-07-08 02:09:17 -0400

    added a new branch and starting over

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express')
-const userRouter = require('./routes/user.routes')
 
-//initializations
-const PORT = process.env.PORT || 3333
+// initialization
+
+const PORT = process.env || 8000
 const app = express()
 
-app.listen(PORT, () => {
-    console.log(`lets get to the grizzy: ${PORT}`)
+app.listen(PORT, () =>{
+    console.log(`lets get to work ${PORT}`)
 })
 
 //middleware
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
-//Routes
-app.use('/user', userRouter)
-
-
 
 
 module.exports = app
+
+
+
+
+
